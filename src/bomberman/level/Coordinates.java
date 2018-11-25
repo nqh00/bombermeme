e1750ccbd421d2462a7109d5c8a9e3e1800254e4
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 00:30:49 +0700

    Coordinates of pixel, coordinates of tiles

--- src/bomberman/level/Coordinates.java
+++ src/bomberman/level/Coordinates.java
@@ -6,9 +6,31 @@
 
 package bomberman.level;
 
+import bomberman.Game;
+
 /**
  * Lấy tọa độ của thực thể cố định trên bản đồ
  */
 public class Coordinates {
-
+	
+	/**
+	 * Chuyển đổi tọa độ pixel thành các khối
+	 */
+	public static int pixelToTile(double i) {
+		return (int)(i / Game.TILES_SIZE);
+	}
+	
+	/**
+	 * Chuyển đổi tọa độ các khối thành pixel
+	 */
+	public static int tileToPixel(int i) {
+		return i * Game.TILES_SIZE;
+	}
+	
+	/**
+	 * Chuyển đổi tọa các khối thành pixel
+	 */
+	public static int tileToPixel(double i) {
+		return (int)(i * Game.TILES_SIZE);
+	}
 }
