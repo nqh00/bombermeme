4b1fd39b3f855f63eeddc2c2fd511bf6ec6f32b6
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 01:04:08 +0700

    Handle sound effect for the game

--- src/bomberman/sound/Sound.java
+++ src/bomberman/sound/Sound.java
@@ -6,9 +6,57 @@
 
 package bomberman.sound;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  * Xử lý âm thanh cho trò chơi
  */
 public class Sound {
+private Clip clip;
 	
+	/**
+	 * Khai báo đường dẫn cho âm thanh
+	 * @param path
+	 */
+	public Sound(File path) {
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(path);
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Phát âm thanh
+	 */
+	public void play() {
+		clip.start();
+	}
+	
+	/**
+	 * Dừng âm thanh
+	 */
+	public void stop() {
+		clip.stop();
+	}
+	
+	/**
+	 * Lặp âm thanh
+	 */
+	public void loop() {
+		clip.loop(-1);
+	}
 }
