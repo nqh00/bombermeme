e31ab25517f10e437c2255456eb202b02ccaa0f6
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 01:16:12 +0700

    Entity with animation

--- src/bomberman/entities/AnimatedEntity.java
+++ src/bomberman/entities/AnimatedEntity.java
@@ -10,5 +10,16 @@ package bomberman.entities;
  * Thực thể có hiệu ứng hoạt hình
  */
 public abstract class AnimatedEntity extends Entity {
-
+	protected int _animate = 0;
+	protected final int MAX_ANIMATE = 7500;
+	
+	/**
+	 * Tạo hiệu ứng hoạt hình cho thực thể
+	 */
+	protected void animate() {
+		if(_animate < MAX_ANIMATE)
+			_animate++;
+		else
+			_animate = 0;
+	}
 }
