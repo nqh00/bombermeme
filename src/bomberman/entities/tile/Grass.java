f74670d59510ec43664604c810f6f25b07aa69b8
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 20:13:56 +0700

    Grass, entities that can be passed through by all entity

--- src/bomberman/entities/tile/Grass.java
+++ src/bomberman/entities/tile/Grass.java
@@ -6,9 +6,24 @@
 
 package bomberman.entities.tile;
 
+import bomberman.entities.Entity;
+import bomberman.graphics.Sprite;
+
 /**
  * Thể hiện của thực thể Grass
  */
 public class Grass extends Tile {
-
+	
+	/**
+	 * Hiển thị hình ảnh của thực thể Grass
+	 */
+	public Grass(int x, int y, Sprite sprite) {
+		super(x, y, sprite);
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		// TODO: Cho mọi đối tượng khác đi qua
+		return true;
+	}
 }
