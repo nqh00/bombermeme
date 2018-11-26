6fd4ca577dd69efe7784c6adcfedf5b609cb23dd
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-27 02:09:35 +0700

    Here is the main

--- src/bomberman/Bomberman.java
+++ src/bomberman/Bomberman.java
@@ -6,8 +6,11 @@
 
 package bomberman;
 
+import bomberman.exceptions.GameException;
+import bomberman.gui.Frame;
+
 public class Bomberman {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws GameException {
+		new Frame();
 	}
 }
