5a303d28b05d33752f8f76e04e191830d229678f
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-27 02:38:00 +0700

    Initialize sprite for FlameItem, handle event while Bomber walked through this item

--- src/bomberman/entities/tile/item/FlameItem.java
+++ src/bomberman/entities/tile/item/FlameItem.java
@@ -6,9 +6,38 @@
 
 package bomberman.entities.tile.item;
 
+import bomberman.Game;
+import bomberman.entities.Entity;
+import bomberman.entities.character.Bomber;
+import bomberman.graphics.Sprite;
+
 /**
  * Thể hiện của vật phẩm FlameItem
  */
 public class FlameItem extends Item {
+	
+	/**
+	 * Hiển thị hình ảnh thực thể FlameItem
+	 */
+	public FlameItem(int x, int y, int level, Sprite sprite) {
+		super(x, y, level, sprite);
+	}
 
+	@Override
+	public void setValue() {
+		_active = true;
+		Game.addBombRadius(1);
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		// TODO: Xử lý khi Bomber ăn vật phẩm FlameItem
+		if(e instanceof Bomber) {
+			((Bomber)e).addItem(this);
+			remove();
+			return true;
+		}
+		
+		return false;
+	}
 }
