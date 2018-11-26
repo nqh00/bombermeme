d67c13d3e7d9a903a26febb5e34ce50952004bbe
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-27 02:13:59 +0700

    Here is where game displayed

--- src/bomberman/gui/GamePanel.java
+++ src/bomberman/gui/GamePanel.java
@@ -6,11 +6,38 @@
 
 package bomberman.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
+
+import bomberman.Game;
+import bomberman.exceptions.GameException;
 
 /**
  * Swing Panel chứa cảnh game
  */
 public class GamePanel extends JPanel {
+	private Game _game;
 	
+	public GamePanel(Frame frame) {
+		setLayout(new BorderLayout());
+		setPreferredSize(new Dimension(Game.WIDTH * Game.SCALE, Game.HEIGHT * Game.SCALE));
+
+		try {
+			_game = new Game(frame);
+			add(_game);
+			_game.setVisible(true);
+		} catch (GameException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		setVisible(true);
+		setFocusable(true);
+	}
+	
+	public Game getGame() {
+		return _game;
+	}
 }
