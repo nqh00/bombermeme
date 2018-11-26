3f1640064a4199020cebc6f09f6364b617ae15dd
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-27 02:38:58 +0700

    Initialize sprite for SpeedItem, handle event while Bomber walked through this item

--- src/bomberman/entities/tile/item/SpeedItem.java
+++ src/bomberman/entities/tile/item/SpeedItem.java
@@ -6,9 +6,37 @@
 
 package bomberman.entities.tile.item;
 
+import bomberman.Game;
+import bomberman.entities.Entity;
+import bomberman.entities.character.Bomber;
+import bomberman.graphics.Sprite;
+
 /**
  * Thể hiện của vật phẩm SpeedItem
  */
 public class SpeedItem extends Item {
+	
+	/**
+	 * Hiển thị hình ảnh thực thể SpeedItem
+	 */
+	public SpeedItem(int x, int y, int level, Sprite sprite) {
+		super(x, y, level, sprite);
+	}
 
+	@Override
+	public void setValue() {
+		_active = true;
+		Game.addBomberSpeed(0.1);
+	}
+	
+	@Override
+	public boolean collide(Entity e) {
+		// TODO: Xử lý khi Bomber ăn vật phẩm SpeedItem
+		if(e instanceof Bomber) {
+			((Bomber)e).addItem(this);
+			remove();
+			return true;
+		}
+		return false;
+	}
 }
