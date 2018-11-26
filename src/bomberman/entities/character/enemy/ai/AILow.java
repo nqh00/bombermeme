206d9afe73ba158575d6c63bcde07a2957a2188f
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 22:16:42 +0700

    Random 4 direction then call it artificial-intelligence

--- src/bomberman/entities/character/enemy/ai/AILow.java
+++ src/bomberman/entities/character/enemy/ai/AILow.java
@@ -10,5 +10,10 @@ package bomberman.entities.character.enemy.ai;
  * Thuật toán tìm đường đi đơn giản
  */
 public class AILow extends AI {
-
+	
+	@Override
+	public int calculateDirection() {
+		// TODO: Cài đặt thuật toán tìm đường đi
+		return random.nextInt(4);
+	}
 }
