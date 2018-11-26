265f2c6d33a2e43478aabf5167aaac9eb93ae5bf
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 22:13:24 +0700

    Calculate direction for enemy by randomize direction so-called A.I

--- src/bomberman/entities/character/enemy/ai/AI.java
+++ src/bomberman/entities/character/enemy/ai/AI.java
@@ -6,9 +6,17 @@
 
 package bomberman.entities.character.enemy.ai;
 
+import java.util.Random;
+
 /**
  * Thuật toán tìm đường đi bao gồm tìm đường đi đơn giản và tìm đường đi nâng cao
  */
 public abstract class AI {
+	protected Random random = new Random();
 
+	/**
+	 * Thuật toán tìm đường đi
+	 * @return hướng đi xuống/phải/trái/lên tương ứng với các giá trị 0/1/2/3
+	 */
+	public abstract int calculateDirection();
 }
