942f2186c38fc591e86a2ee00c8f94b6b40775fc
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 20:18:26 +0700

    Wall, entities that cannot be passed through by any entity

--- src/bomberman/entities/tile/Wall.java
+++ src/bomberman/entities/tile/Wall.java
@@ -6,9 +6,17 @@
 
 package bomberman.entities.tile;
 
+import bomberman.graphics.Sprite;
+
 /**
  * Thể hiện của thực thể Wall
  */
 public class Wall extends Tile {
 
+	/**
+	 * Hiển thị hình ảnh của thực thể Wall
+	 */
+	public Wall(int x, int y, Sprite sprite) {
+		super(x, y, sprite);
+	}
 }
