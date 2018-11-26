81c6d78c94af8181890483a28bb639a056e02a7b
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-26 21:42:37 +0700

    Item, entities cannot be passed through by enemy, if bomber pass through, item will disappear, then ability of bomber will be adjusted

--- src/bomberman/entities/tile/item/Item.java
+++ src/bomberman/entities/tile/item/Item.java
@@ -7,10 +7,57 @@
 package bomberman.entities.tile.item;
 
 import bomberman.entities.tile.Tile;
+import bomberman.graphics.Sprite;
 
 /** 
  * Bao gồm các vật phẩm: BombItem, FlameItem và SpeedItem
  */
 public abstract class Item extends Tile {
+	protected int _duration = -1;	//-1 là luôn có hiệu lực
+	protected boolean _active = false;
+	protected int _level;
 	
+	/**
+	 * Hiển thị hình ảnh thực thể Item cố định
+	 * @param x
+	 * @param y
+	 * @param sprite
+	 */
+	public Item(int x, int y, int level, Sprite sprite) {
+		super(x, y, sprite);
+		_level = level;
+	}
+	
+	/**
+	 * Setter: giá trị của Item
+	 */
+	public abstract void setValue();
+	
+	public void removeLive() {
+		if(_duration > 0) 
+			_duration--;
+		if(_duration == 0)
+			_active = false;
+	}
+
+	/**
+	 * Getter: thời gian hiệu lực
+	 */
+	public int getDuration() {
+		return _duration;
+	}
+
+	/**
+	 * Getter: kiểm tra hiệu lực
+	 */
+	public boolean isActive() {
+		return _active;
+	}
+
+	/**
+	 * Getter: thông tin màn chơi
+	 */
+	public int getLevel() {
+		return _level;
+	}
 }
