fdcab25615b9812f2d04f14dfe035bef51d9c9a1
Nguyen Quang Huy <nqhb00@gmail.com>
2018-11-27 02:15:41 +0700

    Here is what everthing display in

--- src/bomberman/gui/Frame.java
+++ src/bomberman/gui/Frame.java
@@ -6,11 +6,62 @@
 
 package bomberman.gui;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import bomberman.Game;
 
 /**
  * Swing Frame chứa toàn bộ các component
  */
 public class Frame extends JFrame {
+	private JPanel _containerpanel;
+	private GamePanel _gamepanel;
+	private InfoPanel _infopanel;
 	
+	private Game _game;
+
+	public Frame() {
+		_containerpanel = new JPanel(new BorderLayout());
+		_gamepanel = new GamePanel(this);
+		_infopanel = new InfoPanel(_gamepanel.getGame());
+		
+		_containerpanel.add(_infopanel, BorderLayout.PAGE_START);
+		_containerpanel.add(_gamepanel, BorderLayout.PAGE_END);
+		
+		_game = _gamepanel.getGame();
+		
+		add(_containerpanel);
+		
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);	
+		
+		_game.start();
+	}
+	
+	/**
+	 * Setter: cập nhật thời gian
+	 */
+	public void setTime(int time) {
+		_infopanel.setTime(time);
+	}
+	
+	/**
+	 * Setter: cập nhật điểm
+	 */
+	public void setPoints(int points) {
+		_infopanel.setPoints(points);
+	}
+	
+	/**
+	 * Setter: cập nhật mạng
+	 */
+	public void setLive(int lives) {
+		_infopanel.setLives(lives);
+	}
 }
